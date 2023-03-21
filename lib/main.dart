337cf8558b5c0ec6201a9c622a6a8c9f0f78960e
Productos-Union <manager@union.pe>
2023-03-21 11:00:41 -0500

    count steps

---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:pedometer/pedometer.dart';
+
+String formatDate(DateTime d) {
+  return d.toString().substring(0, 19);
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void onStepCount(StepCount event) {
+    print("Estoy al inicio event $event");
+    setState(() {
+      _steps = event.steps.toString();
+      print("Estoy dentro de setState $_steps");
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    print("Aqui empezamos el event");
+    print(event);
+    setState(() {
+      _status = event.status;
+    print("Aqui empezamos el _status $_status");
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    print('Aqui es donde pasa algun error posible: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print("Aqui imprimimos un error $_status");
+  }
+
+  void onStepCountError(error) {
+    print("Aqui empezamos otro error para ver que paso");
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void initPlatformState() {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("Yo soy steps pe causa $_steps");
+    print("Yo soy status pe causa $_status");
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pedometer example app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Steps taken:',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                _steps,
+                style: const TextStyle(fontSize: 60),
+              ),
+              const Divider(
+                height: 100,
+                thickness: 0,
+                color: Colors.white,
+              ),
+              const Text(
+                'Pedestrian status:',
+                style: TextStyle(fontSize: 30),
+              ),
+              Icon(
+                _status == 'walking'
+                    ? Icons.directions_walk
+                    : _status == 'stopped'
+                        ? Icons.accessibility_new
+                        : Icons.error,
+                size: 100,
+              ),
+              Center(
+                child: Text(
+                  _status,
+                  style: _status == 'walking' || _status == 'stopped'
+                      ? const TextStyle(fontSize: 30)
+                      : const TextStyle(fontSize: 20, color: Colors.red),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
